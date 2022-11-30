83cf0f447c3cf0fe7423043abb9f59b41cc1327d
Imene SI SERIR <imenesiserir@gmail.com>
2022-11-30 14:46:57 +0100

    commit_zero

--- src/MainApp.java
+++ src/MainApp.java
@@ -5,7 +5,7 @@ public class MainApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-
+// Je vais modifier ce code pour ajouter mon compte d'étudiant à la BD de la bibliothèque numérique
 		EtudiantService serv=new EtudiantService();
 		try {
 			serv.inscription(2, "Guendouziiiii", "wassila", "guen@gmail.com","xxxx", 1);
